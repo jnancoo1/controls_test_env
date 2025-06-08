5022e4f7be7a25dc20f245b12f86f95b6d43ca8f
j_nancoo <jnancoo123@gmail.com>
2025-06-08 19:55:38 +0000

    synced  w master

---
+++ b/CMakeFiles/unit_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_test.dir/src/matrix_math_test.cpp.o"
+  "CMakeFiles/unit_test.dir/src/matrix_math_test.cpp.o.d"
+  "unit_test"
+  "unit_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ b/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/j_nancoo/controls_library/gtest/googlemock
+# Build directory: /home/j_nancoo/controls_library/b/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
