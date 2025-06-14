ec2a3487cb85546bbe514d7924ca6f85ca092882
j_nancoo <jnancoo123@gmail.com>
2025-06-14 20:35:34 +0000

    cleanup

--- b/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/j_nancoo/controls_library/gtest
-# Build directory: /home/j_nancoo/controls_library/b/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
